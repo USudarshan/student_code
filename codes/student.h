bbe556cb87cd83c3486f4ad8a50d40194120b641
dac <dac@dypm.org>
2023-10-21 15:04:55 +0530

    success

---
+++ codes/student.h
@@ -0,0 +1,20 @@
+#include<stdio.h>
+using namespace std;
+
+class student{
+	protected: int id;
+	string name;
+	double m1,m2,m3;
+	
+//	public: void caltotalmarks(m1,m2,m3){
+//		total=m1+m2+m3
+//		cout<<"total marks ="<<total<<endl;
+//		
+//	
+//	}
+//		void percentage(total){
+//			per=(total/300)*100;
+//			cout<<"percentage is :"<<per
+//		}
+
+};
